e449e7290c9b4334fe49077c696d54c9affffcce
hin <hin@cvs.openbsd.org>
2002-02-06 08:55:55 +0000

    Import of heimdal-0.4e

--- kerberosV/src/lib/roken/setprogname.c
+++ kerberosV/src/lib/roken/setprogname.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995-2004 Kungliga Tekniska Högskolan 
+ * Copyright (c) 1995 - 2001 Kungliga Tekniska Högskolan 
  * (Royal Institute of Technology, Stockholm, Sweden).  
  * All rights reserved.
  * 
@@ -33,7 +33,7 @@
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-RCSID("$KTH: setprogname.c,v 1.3 2005/04/12 11:29:05 lha Exp $");
+RCSID("$KTH: setprogname.c,v 1.1 2001/07/09 14:56:51 assar Exp $");
 #endif
 
 #include "roken.h"
@@ -43,7 +43,7 @@ extern const char *__progname;
 #endif
 
 #ifndef HAVE_SETPROGNAME
-void ROKEN_LIB_FUNCTION
+void
 setprogname(const char *argv0)
 {
 #ifndef HAVE___PROGNAME
@@ -59,3 +59,9 @@ setprogname(const char *argv0)
 #endif
 }
 #endif /* HAVE_SETPROGNAME */
+
+void
+set_progname(char *argv0)
+{
+    setprogname ((const char *)argv0);
+}
