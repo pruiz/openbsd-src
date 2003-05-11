563989f5e7b03c2734d773efc4dcec84c4b2a503
markus <markus@cvs.openbsd.org>
2003-05-11 21:34:28 +0000

    import 0.9.7b (without idea and rc5)

--- lib/libssl/src/crypto/mem_clr.c
+++ lib/libssl/src/crypto/mem_clr.c
@@ -64,14 +64,12 @@ unsigned char cleanse_ctr = 0;
 void OPENSSL_cleanse(void *ptr, size_t len)
 	{
 	unsigned char *p = ptr;
-	size_t loop = len, ctr = cleanse_ctr;
+	size_t loop = len;
 	while(loop--)
 		{
-		*(p++) = (unsigned char)ctr;
-		ctr += (17 + ((size_t)p & 0xF));
+		*(p++) = cleanse_ctr;
+		cleanse_ctr += (17 + (unsigned char)((int)p & 0xF));
 		}
-	p=memchr(ptr, (unsigned char)ctr, len);
-	if(p)
-		ctr += (63 + (size_t)p);
-	cleanse_ctr = (unsigned char)ctr;
+	if(memchr(ptr, cleanse_ctr, len))
+		cleanse_ctr += 63;
 	}
