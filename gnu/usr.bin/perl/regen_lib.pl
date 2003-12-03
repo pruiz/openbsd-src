05ebc4b19b2d1585acdda1c5af9813b140c7ff4f
millert <millert@cvs.openbsd.org>
2003-12-03 02:44:33 +0000

    perl 5.8.2 from CPAN

--- gnu/usr.bin/perl/regen_lib.pl
+++ gnu/usr.bin/perl/regen_lib.pl
@@ -1,20 +1,19 @@
 #!/usr/bin/perl -w
 use strict;
-use vars qw($Needs_Write $Verbose @Changed);
-use File::Compare;
-use Symbol;
+use vars qw($Is_W32 $Is_OS2 $Is_Cygwin $Is_NetWare $Needs_Write);
+use Config; # Remember, this is running using an existing perl
 
 # Common functions needed by the regen scripts
 
-$Needs_Write = $^O eq 'cygwin' || $^O eq 'os2' || $^O eq 'MSWin32';
-
-$Verbose = 0;
-@ARGV = grep { not($_ eq '-q' and $Verbose = -1) }
-  grep { not($_ eq '-v' and $Verbose = 1) } @ARGV;
-
-END {
-  print STDOUT "Changed: @Changed\n" if @Changed;
+$Is_W32 = $^O eq 'MSWin32';
+$Is_OS2 = $^O eq 'os2';
+$Is_Cygwin = $^O eq 'cygwin';
+$Is_NetWare = $Config{osname} eq 'NetWare';
+if ($Is_NetWare) {
+  $Is_W32 = 0;
 }
+
+$Needs_Write = $Is_OS2 || $Is_W32 || $Is_Cygwin || $Is_NetWare;
 
 sub safer_unlink {
   my @names = @_;
@@ -39,32 +38,8 @@ sub safer_rename_silent {
   rename $from, $to;
 }
 
-sub rename_if_different {
+sub safer_rename {
   my ($from, $to) = @_;
-
-  if (compare($from, $to) == 0) {
-      warn "no changes between '$from' & '$to'\n" if $Verbose > 0;
-      safer_unlink($from);
-      return;
-  }
-  warn "changed '$from' to '$to'\n" if $Verbose > 0;
-  push @Changed, $to unless $Verbose < 0;
   safer_rename_silent($from, $to) or die "renaming $from to $to: $!";
 }
-
-# Saf*er*, but not totally safe. And assumes always open for output.
-sub safer_open {
-    my $name = shift;
-    my $fh = gensym;
-    open $fh, ">$name" or die "Can't create $name: $!";
-    *{$fh}->{SCALAR} = $name;
-    binmode $fh;
-    $fh;
-}
-
-sub safer_close {
-    my $fh = shift;
-    close $fh or die 'Error closing ' . *{$fh}->{SCALAR} . ": $!";
-}
-
 1;
